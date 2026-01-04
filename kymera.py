9183b86c01c0c0ba7ca66c98b4ba7be0e19fb69b
Kymera <aquamatynho@gmail.com>
2026-01-04 12:53:27 -0300

    Update print statement from 'Hello' to 'Goodbye'

--- kymera.py
+++ kymera.py
@@ -28,7 +28,7 @@ VERSAO_ATUAL = "1.0.0"
 LINK_VERSAO_TXT = "https://raw.githubusercontent.com/Kymera-coder/Kymera-loucher/refs/heads/main/version.txt"
 
 # 2. Cole aqui o site para onde o usuário vai se tiver update
-LINK_SITE_DOWNLOAD = "https://github.com/Kymera-coder/Launcher-Kymera"
+LINK_SITE_DOWNLOAD = "https://github.com/Kymera-coder/Kymera-loucher"
 # ==========================================================
 
 ctk.set_appearance_mode("Dark")
@@ -418,6 +418,7 @@ class KymeraApp(ctk.CTk):
 if __name__ == "__main__":
     app = KymeraApp()
     app.mainloop()
+
 
 
 
